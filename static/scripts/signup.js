02c274ea8466da8ea01d706e8fb3c13b6b01f7b9
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2023-11-17 12:38:41 -0500

    commenting

--- static/scripts/signup.js
+++ static/scripts/signup.js
@@ -1,17 +1,20 @@
 window.addEventListener("DOMContentLoaded", async () => {
+    // get email field for email validation
     const emailField = document.getElementById("email");
     emailField.addEventListener("input", validateEmailAddress);
 
+    // get password field for password validation
     const passwordField = document.getElementById("password");
     passwordField.addEventListener("input", validatePassword);
 
+    // get confirm password field for password matching
     const confirmPasswordField = document.getElementById("confirm_password");
     confirmPasswordField.addEventListener("input", validateConfirmPassword);
 });
 
 // define a function to check that the email is valid
 function validateEmailAddress() {
-    // include an email regex for later . . .
+    // eamil regex to validate email address
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const emailField = document.getElementById("email");
     const email = emailField.value;
