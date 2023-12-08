f1ab75cdc98de01c1971f3b384bdc0b5c4cee6cb
hemmerlinaj20 <100312616+hemmerlinaj20@users.noreply.github.com>
2023-12-08 13:24:00 -0500

    Fixed search in flight to serach by city instead of airport code

--- static/scripts/search_flights.js
+++ static/scripts/search_flights.js
@@ -1,5 +1,4 @@
 
-
 window.addEventListener("DOMContentLoaded", async ()=>{
     // Add event listener to the search btn
     const searchBtn = document.getElementById("search-btn");
@@ -17,7 +16,8 @@ async function getFlights(){
     // API connection stuff
     const RAPIDAPI_KEY = "f34f0f10c2msh6855d6d77950b00p18a515jsn9e45dbeb030f"
     const RAPIDAPI_HOST = "booking-com15.p.rapidapi.com"
-    const RAPIDAPI_BASE_URL = "https://booking-com15.p.rapidapi.com/api/v1/flights/searchFlights"
+
+    const RAPIDAPI_BASE_URL = "https://booking-com15.p.rapidapi.com/api/v1/flights/"
 
     const headers = {
         "X-RapidAPI-Key": RAPIDAPI_KEY,
@@ -25,33 +25,54 @@ async function getFlights(){
     }
 
     // Get the data from the form in the page
-    const from_id = document.getElementById("fromId").value;
-    const to_id = document.getElementById("toId").value;
+    // Get the searched cities
+    const from_city = document.getElementById("fromId").value;
+    const to_city = document.getElementById("toId").value;
+    //TODO: get the airport ids from the cities
+    const getAirportURL = `${RAPIDAPI_BASE_URL}searchDestination?query=`;
+
+    // Get the from airport id
+    let getAirportResponse = await fetch(`${getAirportURL}${from_city}`, {
+        method: "GET",
+        headers: headers
+    });
+    const fromAirportData = await validateJSON(getAirportResponse);
+    const from_id = fromAirportData.data[0].id;
+
+    // Get the to airport id
+    getAirportResponse = await fetch(`${getAirportURL}${to_city}`, {
+        method: "GET",
+        headers: headers
+    });
+    const toAirportData = await validateJSON(getAirportResponse);
+    const to_id = toAirportData.data[0].id;
+
+    //Get the rest of the form data
     const depart_date = document.getElementById("departDate").value;
     const return_date = document.getElementById("returnDate").value;
     const adults = document.getElementById("adults").value;
     const sort_by = document.getElementById("sort").value;
 
     // Construct the URL to get the flight data
-    const URL = `${RAPIDAPI_BASE_URL}?fromId=${from_id}&toId=${to_id}&departDate=${depart_date}&returnDate=${return_date}&pageNo=1&adults=${adults}&children=0&sort=${sort_by}&currency_code=USD`
+    const searchFlightsURL = `${RAPIDAPI_BASE_URL}searchFlights?fromId=${from_id}&toId=${to_id}&departDate=${depart_date}&returnDate=${return_date}&pageNo=1&adults=${adults}&children=0&sort=${sort_by}&currency_code=USD`
 
     // Fetch the flight data from the API
-    const r = await fetch(URL, {
+    const searchFlightResponse = await fetch(searchFlightsURL, {
         method: "GET",
-        headers: headers,
+        headers: headers
     });
 
     // Validate the JSON
-    const data = await validateJSON(r);
-    console.log(data);
+    const searchFlightData = await validateJSON(searchFlightResponse);
+    console.log(searchFlightData);
 
-    if(data.data.error){
+    if(searchFlightData.data.error){
         console.log("not iterable")
         // TODO: Show a message showing no flights
     }
-    
+
     // Fill the table with the flights
-    for(const flight of data.data.flightOffers){
+    for(const flight of searchFlightData.data.flightOffers){
         const tr = document.createElement("tr");
         insertFlight(tr, flight);
         tb.appendChild(tr);
